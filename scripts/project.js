5f7fa2a34cdbf316a64fe663ba780d1724e3f3f6
ValRaz <valerie.dres@gmail.com>
2023-10-24 16:27:13 -0600

    final functionailty push

--- scripts/project.js
+++ scripts/project.js
@@ -14,26 +14,16 @@ const optionsElement = document.getElementById("options");
 const resultElement = document.getElementById("result");
 const scoreElement = document.getElementById("score-value");
 const nextButton = document.getElementById("next");
-const showScoresButton = document.getElementById("show-scores");
-
-//Retrieves high scores from local storage
-let savedHighScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-//Initializes an array to store high scores
-let highScores = savedHighScores;
-
-displayHighScores();
 
 // Initialize the game on page load
 window.addEventListener("load", () => {
     startGame();
 });
 
-//Adds event listeners to each button
+//Adds event listeners to next button
 nextButton.addEventListener("click", () => {
     nextQuestion();
 });
-showScoresButton.addEventListener("click", () => displayHighScores(savedHighScores));
 
 //Starts game, fetches questions and initiates the first question
 function startGame() {
@@ -47,30 +37,39 @@ function startGame() {
 }
 
 //Displays the current question and answer options
-function displayQuestion(savedHighScores) {
+function displayQuestion() {
     if (currentQuestionIndex < questions.length) {
         const question = questions[currentQuestionIndex];
         questionElement.textContent = he.decode(question.question);
         resultElement.textContent = "";
         //Clears previous options
         optionsElement.innerHTML = "";
-        //Creates button for the correct answer
-        const correctButton = document.createElement("button");
-        correctButton.classList.add("btn", "option");
-        correctButton.textContent = question.correct_answer;
-        optionsElement.appendChild(correctButton);
-        correctButton.addEventListener("click", checkAnswer);
-        //Creates buttons for incorrect answers
-        question.incorrect_answers.forEach((incorrectAnswer) => {
+
+        //Combines correct and incorrect answers into a single array
+        const answers = [question.correct_answer, ...question.incorrect_answers];
+        //Shuffles the answers array
+        shuffleArray(answers);
+
+        //Creates button for the shuffled answers
+        const buttons = answers.map((answer) => {
             const button = document.createElement("button");
             button.classList.add("btn", "option");
-            button.textContent = he.decode(incorrectAnswer);
+            button.textContent = he.decode(answer);
+            button.addEventListener("click", checkAnswer);
             optionsElement.appendChild(button);
-            button.addEventListener("click", checkAnswer); 
+            return button;
         });
     }
     else {
         endGame();
+    }
+}
+
+//Function for shuffling an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i> 0; i--) {
+        const j = Math.floor(math.random() * (i+1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
 
@@ -109,37 +108,4 @@ function endGame() {
     optionsElement.innerHTML = "";
     resultElement.textContent = `Your final score is: ${score}`;
     nextButton.style.display = "none";
-
-    //Checks if the current score is higher than any high score currently existing on the array
-    const currentScoreIndex = highScores.findIndex((existingScore) => score > existingScore);
-    if (currentScoreIndex !== -1) {
-        //Replaces the lowest high score with the current score if appropriate
-        highScores.splice(currentScoreIndex, 1, score);
-    }
-
-    //Sorts the array of high scores in ascending order
-    highScores.sort((a, b) => b - a);
-    //Maintains a limit of 10 high scores stored on the array
-    highScores = highScores.slice(0, maxhighScores);
-
-    //Saves the updated high scores array in local storage
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-
-    displayHighScores();
-}
-
-//Displays High Scores from the stored array in an unordered list
-function displayHighScores() {
-    const container = document.querySelector(".container");
-    const existingHighScoresList = document.querySelector(".high-scores");
-
-    if (existingHighScoresList) {
-        container.removeChild(existingHighScoresList);
-    }
-
-    const highScoresList = highScores.map((score, index) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = `#${index + 1}: ${score}`;
-        returnlistItem;
-        });
 }
