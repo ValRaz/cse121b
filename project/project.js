5206706d81c8942811747caa4fc00b9afa60d426
ValRaz <valerie.dres@gmail.com>
2023-10-24 11:57:07 -0600

    filestructure

--- project/project.js
+++ project/project.js
@@ -1,5 +1,5 @@
 //Imports the API url constant from the config module
-import { API_URL } from "./projectconfig";
+import { API_URL } from "./projectconfig.js";
 
 //Instantiates varibale for storing questions, current question index, the score, and sets maximum high scores to ten
 let questions = [];
